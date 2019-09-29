b86088513e4f266d88a3de45ad6612f3adacc380
grif <b_evgeny@ukr.net>
2019-09-29 21:45:59 +0300

    corrected models

--- image_editor_test/src/animation_models/HexagonGreed.ts
+++ image_editor_test/src/animation_models/HexagonGreed.ts
@@ -1,13 +1,26 @@
 import {CustomDraw} from "../CustomDraw";
 import {IPolygon} from "../Screen";
 
-export class Hexagon extends CustomDraw {
+export class HexagonGreed extends CustomDraw {
+    x = 0;
+    y = 0;
+    dx = 1;
+    dy = 1;
+    startDelta = 1300;
+    maxStep = 5;
+    bound = Math.round(this.startDelta / 2);
+    bottomLayerName = 'bottomLayer';
+
     constructor(canvas: HTMLCanvasElement) {
         super(canvas);
+        this.init();
     }
 
-    renderFrame(): void {
-        const startDelta = 1300;
+    setName(name: string): void {
+        this.name = name;
+    }
+
+    init(): void {
         const dx = 45;
         const dy = 75;
         const radius = 45;
@@ -20,9 +33,9 @@ export class Hexagon extends CustomDraw {
             {x: 5, y: 75},
             {x: 5, y: 25},
         ];
-        const bottomLayer = 'bottomLayer';
-        const centerLayer = 'centerLayer';
-        const topLayer = 'topLayer';
+
+        const centerLayerName = 'centerLayer';
+        const topLayerName = 'topLayer';
 
         hexagon.forEach(element => {
             element.x *= multiplier;
@@ -33,31 +46,29 @@ export class Hexagon extends CustomDraw {
         let modDY = dy * multiplier;
         let modRadius = radius * multiplier;
 
-        this.customScreen.setVirtualCanvas(bottomLayer, 2500, 3500);
+        this.customScreen.setVirtualCanvas(this.bottomLayerName, 2500, 3500);
         this.customScreen.setLineWidth(11);
         this.customScreen.setColors('rgb(15,15,50)', 'rgba(0,0,0,0.3)');
         this.createVirtualGreed(modRadius, hexagon, modDX, modDY);
         this.customScreen.restoreCanvas();
 
-        this.customScreen.setVirtualCanvas(centerLayer, 2500, 3500);
+        this.customScreen.setVirtualCanvas(centerLayerName, 2500, 3500);
         this.customScreen.setLineWidth(3);
         this.customScreen.setColors('rgba(100,100,100,0)', 'rgba(255,255,255,0.2)');
         this.createVirtualGreed(modRadius, hexagon, modDX, modDY);
         this.customScreen.restoreCanvas();
 
-        this.customScreen.setVirtualCanvas(topLayer, 2500, 3500);
+        this.customScreen.setVirtualCanvas(topLayerName, 2500, 3500);
         this.customScreen.setLineWidth(3);
         this.customScreen.setColors('rgba(0,100,255,0)', 'rgba(0,0,0,0.5)');
         this.createVirtualGreed(modRadius, hexagon, modDX, modDY);
         this.customScreen.restoreCanvas();
 
-        this.customScreen.drawVirtualOnVirtualCanvas(bottomLayer, centerLayer, 3, 1);
-        this.customScreen.drawVirtualOnVirtualCanvas(bottomLayer, topLayer, 0, 0);
+        this.customScreen.drawVirtualOnVirtualCanvas(this.bottomLayerName, centerLayerName, 3, 1);
+        this.customScreen.drawVirtualOnVirtualCanvas(this.bottomLayerName, topLayerName, 0, 0);
 
-        this.customScreen.deleteVirtualCanvas(centerLayer);
-        this.customScreen.deleteVirtualCanvas(topLayer);
-
-        this.drawCanvases(bottomLayer, startDelta);
+        this.customScreen.deleteVirtualCanvas(centerLayerName);
+        this.customScreen.deleteVirtualCanvas(topLayerName);
     }
 
     private createVirtualGreed(modRadius: number, hexagon: IPolygon, modDX: number, modDY: number) {
@@ -73,47 +84,28 @@ export class Hexagon extends CustomDraw {
         }
     }
 
-    drawCanvases(virtualCanvasName: string, startDelta: number) {
-        let x = 0;
-        let y = 0;
-        let dx = 1;
-        let dy = 1;
-        let mute = false;
-        const maxStep = 5;
-        const bound = Math.round(startDelta / 2);
+    renderFrame() {
+        if (this.x <= 0) {
+            this.dx = this.randomize(this.maxStep);
+        }
+        if (this.x >= this.bound) {
+            this.dx = -1 * this.randomize(this.maxStep);
+        }
+        if (this.y <= 0) {
+            this.dy = this.randomize(this.maxStep);
+        }
+        if (this.y >= this.bound) {
+            this.dy = -1 * this.randomize(this.maxStep);
+        }
 
-        const move = () => {
-            requestAnimationFrame(move);
-            if (mute) {
-                console.log('muted');
-                return;
-            }
-            mute = true;
-            if (x <= 0) {
-                dx = this.randomize(maxStep);
-            }
-            if (x >= bound) {
-                dx = -1 * this.randomize(maxStep);
-            }
-            if (y <= 0) {
-                dy = this.randomize(maxStep);
-            }
-            if (y >= bound) {
-                dy = -1 * this.randomize(maxStep);
-            }
+        if (this.dx === 0 && this.dy === 0) {
+            this.dx = 1 + this.randomize(this.maxStep);
+            this.dy = 1 + this.randomize(this.maxStep);
+        }
 
-            if (dx === 0 && dy === 0) {
-                dx = 1 + this.randomize(maxStep);
-                dy = 1 + this.randomize(maxStep);
-            }
-
-            this.customScreen.clear();
-            this.customScreen.drawVirtualOnRealCanvas(virtualCanvasName, -startDelta + x, -startDelta + y);
-            x += dx;
-            y += dy;
-            mute = false;
-        };
-        move();
+        this.customScreen.drawVirtualOnRealCanvas(this.bottomLayerName, -this.startDelta + this.x, -this.startDelta + this.y);
+        this.x += this.dx;
+        this.y += this.dy;
     }
 
     getModified(hexagon: IPolygon, deltaX = 0, deltaY = 0): IPolygon {
--- image_editor_test/src/animation_models/MovedCircle.ts
+++ image_editor_test/src/animation_models/MovedCircle.ts
@@ -1,53 +1,53 @@
-import {CustomDraw} from "./CustomDraw";
+import {CustomDraw} from "../CustomDraw";
 
 export class MovedCircle extends CustomDraw {
+    x = 0;
+    y = 0;
+    dx = 1;
+    dy = 1;
+    radius = 50;
+    maxStep = 15;
+    radiusCalc = (this.radius + 5) * 2;
+    bottomLayerName = 'circle';
 
     constructor(canvas: HTMLCanvasElement) {
         super(canvas);
+        this.init();
     }
 
-    start(): void {
-        let x = 0;
-        let y = 0;
-        let dx = 1;
-        let dy = 1;
-        let mute = false;
-        const radius = 50;
-        const maxStep = 15;
-        const radiusCalc = radius + 5;
+    setName(name: string): void {
+        this.name = name;
+    }
 
-        const move = () => {
-            requestAnimationFrame(move);
-            if (mute) {
-                console.log('muted');
-                return;
-            }
-            mute = true;
-            if (x <= radiusCalc) {
-                dx = this.randomize(maxStep);
-            }
-            if (x >= this.customCanvas.width - radiusCalc) {
-                dx = -1 * this.randomize(maxStep);
-            }
-            if (y <= radiusCalc) {
-                dy = this.randomize(maxStep);
-            }
-            if (y >= this.customCanvas.height - radiusCalc) {
-                dy = -1 * this.randomize(maxStep);
-            }
+    init() {
+        this.customScreen.setVirtualCanvas(this.bottomLayerName, 120, 120);
+        this.customScreen.setLineWidth(10);
+        this.customScreen.setColors('yellow', '#825000');
+        this.customScreen.drawSimpleCircle(this.radius + 5, this.radius + 5, this.radius);
+        this.customScreen.restoreCanvas();
+    }
 
-            if (dx === 0 && dy === 0) {
-                dx = 1 + this.randomize(maxStep);
-                dy = 1 + this.randomize(maxStep);
-            }
+    renderFrame(): void {
+        if (this.x <= 0) {
+            this.dx = this.randomize(this.maxStep);
+        }
+        if (this.x >= this.customCanvas.width - this.radiusCalc) {
+            this.dx = -1 * this.randomize(this.maxStep);
+        }
+        if (this.y <= 0) {
+            this.dy = this.randomize(this.maxStep);
+        }
+        if (this.y >= this.customCanvas.height - this.radiusCalc) {
+            this.dy = -1 * this.randomize(this.maxStep);
+        }
 
-            this.customScreen.clear();
-            this.customScreen.setColors('yellow', 'red');
-            this.customScreen.drawSimpleCircle(x, y, radius);
-            x += dx;
-            y += dy;
-            mute = false;
-        };
-        move();
+        if (this.dx === 0 && this.dy === 0) {
+            this.dx = 1 + this.randomize(this.maxStep);
+            this.dy = 1 + this.randomize(this.maxStep);
+        }
+
+        this.customScreen.drawVirtualOnRealCanvas(this.bottomLayerName, this.x, this.y);
+        this.x += this.dx;
+        this.y += this.dy;
     }
 }
