d68e1ff3db7feb3f9bc2209787403404c51b74c6
Evgeny <b_evgeny@ukr.net>
2019-10-03 11:18:42 +0300

    Add to do list

---
+++ image_editor_test/src/TODO_LIST.ts
@@ -0,0 +1,4 @@
+// TODO 1) Create project documentation
+// TODO 2) Create reverse and guided animation (set custom stop and start frames)
+// TODO 3) Add events to animation models (mouse over, click and t.e.)
+// TODO 4) Create effective background animation (move on left, right and t.e. via virtual canvases and custom map of dimensions)
