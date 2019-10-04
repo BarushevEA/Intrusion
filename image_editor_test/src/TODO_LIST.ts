5058747ac68d3b9e3788aa30a126a87cce91ca1c
grif <b_evgeny@ukr.net>
2019-10-05 00:20:07 +0300

    Update to do list

--- image_editor_test/src/TODO_LIST.ts
+++ image_editor_test/src/TODO_LIST.ts
@@ -1,5 +1,6 @@
 // TODO 1) Create project documentation
 // DONE TODO 2) Create reverse and guided animation (set custom stop and start frames)
-// TODO 3) Add events to animation models (mouse over, click and t.e.)
-// TODO 3.1) create effective observable un subscribe
+// DONE TODO 3) Add events to animation models (mouse over, click)
+// DONE TODO 3.1) create effective observable un subscribe
 // TODO 4) Create effective background animation (move on left, right and t.e. via virtual canvases and custom map of dimensions)
+// TODO 5) Create combined group component
