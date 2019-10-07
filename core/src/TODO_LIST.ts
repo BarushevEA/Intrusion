7ef49e06951ef54f596600741641372a587313c0
Evgeny <b_evgeny@ukr.net>
2019-10-07 11:52:08 +0300

    Update to do list

--- core/src/TODO_LIST.ts
+++ core/src/TODO_LIST.ts
@@ -1,6 +1,7 @@
 // TODO 1) Create project documentation
 // DONE TODO 2) Create reverse and guided animation (set custom stop and start frames)
-// DONE TODO 3) Add events to animation models (mouse over, click)
+// DONE TODO 3) Add events to animation models (mouse over, click and t.e)
 // DONE TODO 3.1) create effective observable un subscribe
 // TODO 4) Create effective background animation (move on left, right and t.e. via virtual canvases and custom map of dimensions)
-// TODO 5) Create combined group component
+// DONE TODO 5) Create combined group component (created test example)
+// TODO 6) Need to add svg image handler
