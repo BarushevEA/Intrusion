f84fa273272b1111aa91228bb745b9ab1b7150d1
grif <b_evgeny@ukr.net>
2019-10-30 19:48:14 +0200

    add class CoordinatesConverter

--- core/src/AnimationCore/CustomeLibraries/FunctionLibs.ts
+++ core/src/AnimationCore/CustomeLibraries/FunctionLibs.ts
@@ -1,3 +1,8 @@
+export type ICoordinatesConverter = {
+    x(x: number): number;
+    y(y: number): number;
+}
+
 export function findElementOnArray(arr: any[], element: any): number {
     for (let i = 0, k = arr.length - 1; i < arr.length; i++, k--) {
         if (arr[i] === element) {
@@ -8,4 +13,20 @@ export function findElementOnArray(arr: any[], element: any): number {
         }
     }
     return -1;
+}
+
+export class CoordinatesConverter implements ICoordinatesConverter {
+    private readonly canvas: HTMLCanvasElement;
+
+    constructor(canvas: HTMLCanvasElement) {
+        this.canvas = canvas;
+    }
+
+    public x(x: number): number {
+        return x;
+    };
+
+    public y(y: number): number {
+        return this.canvas.height - y;
+    };
 }
