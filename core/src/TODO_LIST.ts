96b9d25c402cc420221e2c23383bbea9ba39bc71
grif <b_evgeny@ukr.net>
2019-11-03 00:29:00 +0200

    Update todo list

--- core/src/TODO_LIST.ts
+++ core/src/TODO_LIST.ts
@@ -2,9 +2,10 @@
 // DONE TODO 2) Create reverse and guided animation (set custom stop and start frames)
 // DONE TODO 3) Add events to animation models (mouse over, click)
 // DONE TODO 3.1) create effective observable un subscribe
-// TODO 3.2) Add events to animation models (mouse key up and key down)
+// DONE TODO 3.2) Add events to animation models (mouse key up and key down)
 // TODO 4) Create effective background animation (move on left, right and t.e. via virtual canvases and custom map of dimensions)
 // DONE TODO 5) Create combined group component (created test example)
 // TODO 6) Need to add svg or Base64 image handler
-// TODO 7) Add drug and drop behavior
-// TODO 7.1) Add to render controller move actor z-index
+// DONE TODO 7) Add drug and drop behavior
+// DONE TODO 7.1) Add to render controller move actor z-index
+// TODO 8) Simplify create, use and develop scenarios
