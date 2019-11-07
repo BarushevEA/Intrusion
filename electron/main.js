0d40410e490ca6d371d6682e5d39aea30d30f1a6
Evgeny <b_evgeny@ukr.net>
2019-11-07 18:45:48 +0200

    Update main bacground

--- electron/main.js
+++ electron/main.js
@@ -11,6 +11,7 @@ function createWindow() {
         height: 670,
         resizable: false,
         show: false,
+        backgroundColor: 'black',
         webPreferences: {
             nodeIntegration: true
         }
