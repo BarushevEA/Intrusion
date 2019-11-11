6cc205f23ae1612959ce5240469ddf8a412a6bc0
grif <b_evgeny@ukr.net>
2019-11-11 02:10:32 +0200

    Update TODO LIST

--- core/src/TODO_LIST.ts
+++ core/src/TODO_LIST.ts
@@ -3,11 +3,12 @@
 // DONE TODO 3) Add events to animation models (mouse over, click)
 // DONE TODO 3.1) create effective observable un subscribe
 // DONE TODO 3.2) Add events to animation models (mouse key up and key down)
-// TODO 3.3) Handle events - right mouse click
+// DONE TODO 3.3) Handle events - right mouse click
 // TODO 4) Create effective background animation (move on left, right and t.e. via virtual canvases and custom map of dimensions)
 // DONE TODO 5) Create combined group component (created test example)
 // TODO 6) Need to add svg or Base64 image handler
 // DONE TODO 7) Add drug and drop behavior
 // DONE TODO 7.1) Add to render controller move actor z-index
 // TODO 8) Simplify create, use and develop scenarios
-// TODO 9) Upgrade destroy behavior (have to clean all)
+// DONE TODO 9) Upgrade destroy behavior (have to clean all)
+// TODO 10) Need to move tick counter from Abstract actor ro render controller
