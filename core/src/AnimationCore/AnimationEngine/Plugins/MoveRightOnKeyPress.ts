226e2b83cde0ac65d1801dea92e8964bac38a970
grif <b_evgeny@ukr.net>
2019-11-24 01:29:58 +0200

    Refactoring move plugins

--- core/src/AnimationCore/AnimationEngine/Plugins/MoveRightOnKeyPress.ts
+++ core/src/AnimationCore/AnimationEngine/Plugins/MoveRightOnKeyPress.ts
@@ -1,69 +1,18 @@
-import {AbstractActorPlugin} from "./AbstractActorPlugin";
+import {AbstractMoveOnKeyPress} from "./AbstractMoveOnKeyPress";
 import {AbstractScene} from "../rootScenes/AbstractScene";
 import {E_KEY_MOVE_PLUGIN} from "./PluginTypes";
-import {keyDownCode$, keyUpCode$} from "../../Store/EventStore";
-import {IKeyCode} from "../../Store/Types";
-import {ISubscriptionLike, Observable} from "../../Libraries/Observable";
 
-export class MoveRightOnKeyPress extends AbstractActorPlugin {
-    private readonly key = 'Key';
-    private keyUp: ISubscriptionLike = <any>0;
-    private _step: number = 0;
-    private _onKeyDown$ = new Observable<number>(0);
-    private _onKeyUp$ = new Observable<number>(0);
-
+export class MoveRightOnKeyPress extends AbstractMoveOnKeyPress {
     constructor(scene: AbstractScene, key: string, step = 10) {
-        super(E_KEY_MOVE_PLUGIN.MOVE_KEY_RIGHT, scene);
-        this.key += key[0].toUpperCase();
-        this._step = step;
-        this.init();
+        super(E_KEY_MOVE_PLUGIN.MOVE_KEY_RIGHT, scene, key, step);
     }
 
-    private init() {
-        this.scene.collect(
-            keyDownCode$.subscribe((code: IKeyCode) => {
-                    if (code.code === this.key && !this.keyUp) {
-                        this.keyUp = this.scene
-                            .tickCount$
-                            .subscribe(() => {
-                                this.root.xPos += this._step;
-                                this._onKeyDown$.next(this._step);
-                            });
-                    }
-                }
-            ),
-            keyUpCode$.subscribe((code: IKeyCode) => {
-                    if (code.code === this.key && this.keyUp) {
-                        this._onKeyUp$.next(this._step);
-                        this.keyUp.unsubscribe();
-                        this.keyUp = <any>0;
-                    }
-                }
-            )
-        );
-    }
-
-    get onKeyDown$(): Observable<number> {
-        return this._onKeyDown$;
-    }
-
-    get onKeyUp$(): Observable<number> {
-        return this._onKeyUp$;
-    }
-
-    get step(): number {
-        return this._step;
-    }
-
-    set step(value: number) {
-        this._step = value;
-    }
-
-    destroy(): void {
-        super.destroy();
-        this._onKeyDown$.destroy();
-        this._onKeyUp$.destroy();
-        this._onKeyDown$ = <any>0;
-        this._onKeyUp$ = <any>0;
+    protected initOnKeyDown(): void {
+        this.keyUp = this.scene
+            .tickCount$
+            .subscribe(() => {
+                this.root.xPos += this._step;
+                this._onKeyDown$.next(this._step);
+            });
     }
 }
