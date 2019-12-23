47fbfeaa31c16d21652f0594047a06139cfb914b
Evgeny <b_evgeny@ukr.net>
2019-12-23 13:46:38 +0200

    update TODO list

--- core/src/TODO_LIST.ts
+++ core/src/TODO_LIST.ts
@@ -8,6 +8,7 @@
 // TODO 4.1) Optimize background animation to one layer !!!
 // DONE TODO 5) Create combined group component (created test example)
 // TODO 6) Need to add svg or Base64 image handler
+// DONE TODO 6.1) Need to add Base64 image handler
 // DONE TODO 7) Add drug and drop behavior
 // DONE TODO 7.1) Add to render controller move actor z-index
 // TODO 8) Simplify create, use and develop scenarios
