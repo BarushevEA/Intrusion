159c00142733231cea2a40d00f0e79922f2f1b8b
grif <b_evgeny@ukr.net>
2020-02-01 00:08:12 +0200

    Update todo list

--- core/src/TODO_LIST.ts
+++ core/src/TODO_LIST.ts
@@ -5,7 +5,8 @@
 // DONE TODO 3.2) Add events to animation models (mouse key up and key down)
 // DONE TODO 3.3) Handle events - right mouse click
 // DONE TODO 4) Create background animation (move on left, right and t.e. via virtual canvases and custom map of dimensions)
-// TODO 4.1) Optimize background animation to one layer !!!
+// DONE TODO 4.1) Optimize background animation to one layer !!!
+// TODO 4.2) Update background animation
 // DONE TODO 5) Create combined group component (created test example)
 // TODO 6) Need to add svg or Base64 image handler
 // DONE TODO 6.1) Need to add Base64 image handler
@@ -19,3 +20,6 @@
 // DONE TODO 13) Need delete actor from scene !!!
 // DONE TODO 13.1) Need unlink actor from scene !!!
 // TODO 14)  memory leak search
+// TODO 15) ADD SINGLETON tick counter
+// TODO 16) Create virtual dimension plugin
+// TODO 17)
