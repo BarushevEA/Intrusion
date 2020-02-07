2f50290f4476c929bc68ebc2c0c69394bb851514
grif <b_evgeny@ukr.net>
2020-02-07 16:33:42 +0200

    update TickGenerator

--- core/src/AnimationCore/Store/TickGenerator.ts
+++ core/src/AnimationCore/Store/TickGenerator.ts
@@ -1,31 +1,92 @@
-import {ITick} from "./Types";
+import {ITickListeners, ITick, cb_function, delay_ms, id_string} from "./Types";
 import {Observable} from "../Libraries/Observable";
 
+const timeoutListeners: ITickListeners = {};
+const keys: string[] = [];
+
 let tickIndex = <any>0,
-    tickDelay = 100,
+    id = Number.MIN_SAFE_INTEGER,
+    tickDelay = 10,
+    optimizeCounter = 0,
+    optimizeNumber = 1000,
+    tick10$ = new Observable(<any>0),
     tick100$ = new Observable(<any>0),
     tick1000$ = new Observable(<any>0);
 
 class TickGenerator implements ITick {
-    private counter = 0;
+    private counter100 = 0;
+    private counter1000 = 0;
 
     constructor() {
         this.init();
     }
 
-    private init() {
+    private init(): void {
         if (!tickIndex) {
             tickIndex = setInterval(() => {
-                tick100$.next(100);
-                if (!this.counter) {
+                tick10$.next(10);
+                if (!this.counter100) {
+                    tick100$.next(100);
+                }
+                if (!this.counter1000) {
                     tick1000$.next(1000);
                 }
-                this.counter++;
-                if (this.counter >= 10) {
-                    this.counter = 0;
+                this.counter100++;
+                if (this.counter100 >= 100) {
+                    this.counter100 = 0;
                 }
+                this.counter1000++;
+                if (this.counter1000 >= 100) {
+                    this.counter1000 = 0;
+                }
+                this.handleListeners();
             }, tickDelay);
         }
+    }
+
+    private handleListeners(): void {
+        let isNeedToOptimize = false;
+        for (let i = 0; i < keys.length; i++) {
+            const listener = timeoutListeners[keys[i]];
+            if (!listener) {
+                continue;
+            }
+            if (listener.isDestroy) {
+                delete timeoutListeners[keys[i]];
+                isNeedToOptimize = true;
+            } else {
+                if (listener.counter >= listener.delay) {
+                    listener.callback();
+                    delete timeoutListeners[keys[i]];
+                    isNeedToOptimize = true;
+                } else {
+                    listener.counter += tickDelay;
+                }
+            }
+        }
+        this.optimizeKeys(isNeedToOptimize);
+    }
+
+    private optimizeKeys(isNeedToOptimize: boolean): void {
+        if (!isNeedToOptimize) {
+            return;
+        }
+        optimizeCounter++;
+        if (optimizeCounter < optimizeNumber) {
+            return;
+        }
+        optimizeCounter = 0;
+        keys.length = 0;
+        const tmpKeys = Object.keys(timeoutListeners);
+        const length = tmpKeys.length;
+        for (let i = 0; i < length; i++) {
+            keys.push(<string>tmpKeys.pop());
+        }
+        tmpKeys.length = 0;
+    }
+
+    get tick10$(): Observable<any> {
+        return tick10$;
     }
 
     get tick100$(): Observable<any> {
@@ -36,8 +97,21 @@ class TickGenerator implements ITick {
         return tick1000$;
     }
 
+    execute(cb: cb_function, time: delay_ms): id_string {
+        const key = '' + (++id);
+        timeoutListeners[key] = {
+            counter: 0,
+            delay: time,
+            callback: cb,
+            isDestroy: false
+        };
+        keys.push(key);
+        return key;
+    };
+
     destroy(): void {
-        this.counter = 0;
+        this.counter100 = 0;
+        this.counter1000 = 0;
         clearInterval(tickIndex);
         tickIndex = <any>0;
         if (tick100$) {
--- core/src/AnimationCore/Store/Types.ts
+++ core/src/AnimationCore/Store/Types.ts
@@ -12,8 +12,20 @@ export type ISize = {
     height: number;
     width: number;
 };
+export type delay_ms = number;
+export type id_string = string;
+export type cb_function = () => void;
+export type ITickListener = {
+    counter: number;
+    delay: delay_ms;
+    callback: cb_function;
+    isDestroy: boolean;
+}
+export type ITickListeners = { [id: string]: ITickListener; }
 export type ITick = {
+    tick10$: Observable<any>;
     tick100$: Observable<any>;
     tick1000$: Observable<any>;
+    execute(cb: cb_function, time: delay_ms): id_string;
     destroy(): void;
 }
