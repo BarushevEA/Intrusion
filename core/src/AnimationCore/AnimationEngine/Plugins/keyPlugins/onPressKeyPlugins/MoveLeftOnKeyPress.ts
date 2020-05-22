212a9519b5b30872b328b4c266884f6364f19b26
Grif <b_evgeny@ukr.net>
2020-05-23 01:24:59 +0300

    group key plugins

--- core/src/AnimationCore/AnimationEngine/Plugins/keyPlugins/onPressKeyPlugins/MoveLeftOnKeyPress.ts
+++ core/src/AnimationCore/AnimationEngine/Plugins/keyPlugins/onPressKeyPlugins/MoveLeftOnKeyPress.ts
@@ -1,6 +1,6 @@
-import {AbstractScene} from "../../rootScenes/AbstractScene";
-import {E_KEY_MOVE_PLUGIN} from "../root/PluginTypes";
-import {AbstractActionOnKeyPress} from "../root/AbstractActionOnKeyPress";
+import {AbstractScene} from "../../../rootScenes/AbstractScene";
+import {E_KEY_MOVE_PLUGIN} from "../../root/PluginTypes";
+import {AbstractActionOnKeyPress} from "../../root/AbstractActionOnKeyPress";
 
 export class MoveLeftOnKeyPress extends AbstractActionOnKeyPress {
     constructor(scene: AbstractScene, key: string, step = 10) {
--- core/src/AnimationCore/AnimationEngine/Plugins/keyPlugins/onPressKeyPlugins/MoveRightOnKeyPress.ts
+++ core/src/AnimationCore/AnimationEngine/Plugins/keyPlugins/onPressKeyPlugins/MoveRightOnKeyPress.ts
@@ -1,6 +1,6 @@
-import {AbstractActionOnKeyPress} from "../root/AbstractActionOnKeyPress";
-import {AbstractScene} from "../../rootScenes/AbstractScene";
-import {E_KEY_MOVE_PLUGIN} from "../root/PluginTypes";
+import {AbstractActionOnKeyPress} from "../../root/AbstractActionOnKeyPress";
+import {AbstractScene} from "../../../rootScenes/AbstractScene";
+import {E_KEY_MOVE_PLUGIN} from "../../root/PluginTypes";
 
 export class MoveRightOnKeyPress extends AbstractActionOnKeyPress {
     constructor(scene: AbstractScene, key: string, step = 10) {
