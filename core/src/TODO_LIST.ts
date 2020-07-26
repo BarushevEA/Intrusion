c8525767679fea35c32513c1c695e42dfafdbbdd
Grif <b_evgeny@ukr.net>
2020-07-27 01:31:01 +0300

    Cleaning TodoList

--- core/src/TODO_LIST.ts
+++ core/src/TODO_LIST.ts
@@ -1,26 +1,26 @@
 // TODO 1) Create project documentation
-// DONE TODO 2) Create reverse and guided animation (set custom stop and start frames)
-// DONE TODO 3) Add events to animation models (mouse over, click)
-// DONE TODO 3.1) create effective observable un subscribe
-// DONE TODO 3.2) Add events to animation models (mouse key up and key down)
-// DONE TODO 3.3) Handle events - right mouse click
-// DONE TODO 3.4) Update observable
-// DONE TODO 4) Create background animation (move on left, right and t.e. via virtual canvases and custom map of dimensions)
-// DONE TODO 4.1) Optimize background animation to one layer !!!
+// DONE 2) Create reverse and guided animation (set custom stop and start frames)
+// DONE 3) Add events to animation models (mouse over, click)
+// DONE 3.1) create effective observable un subscribe
+// DONE 3.2) Add events to animation models (mouse key up and key down)
+// DONE 3.3) Handle events - right mouse click
+// DONE 3.4) Update observable
+// DONE 4) Create background animation (move on left, right and t.e. via virtual canvases and custom map of dimensions)
+// DONE 4.1) Optimize background animation to one layer !!!
 // TODO 4.2) Update background animation
-// DONE TODO 5) Create combined group component (created test example)
+// DONE 5) Create combined group component (created test example)
 // TODO 6) Need to add svg or Base64 image handler
-// DONE TODO 6.1) Need to add Base64 image handler
-// DONE TODO 7) Add drug and drop behavior
-// DONE TODO 7.1) Add to render controller move actor z-index
+// DONE 6.1) Need to add Base64 image handler
+// DONE 7) Add drug and drop behavior
+// DONE 7.1) Add to render controller move actor z-index
 // TODO 8) Simplify create, use and develop scenarios
-// DONE TODO 9) Upgrade destroy behavior (have to clean all)
-// DONE TODO 10) Need to move tick counter from Abstract actor ro render controller
-// DONE TODO 11) Need create a class to handle the cursor changes to "pointer" and "default"
-// DONE TODO 12) Need create a plugin system (plugins and plugin docker)
-// DONE TODO 13) Need delete actor from scene !!!
-// DONE TODO 13.1) Need unlink actor from scene !!!
+// DONE 9) Upgrade destroy behavior (have to clean all)
+// DONE 10) Need to move tick counter from Abstract actor ro render controller
+// DONE 11) Need create a class to handle the cursor changes to "pointer" and "default"
+// DONE 12) Need create a plugin system (plugins and plugin docker)
+// DONE 13) Need delete actor from scene !!!
+// DONE 13.1) Need unlink actor from scene !!!
 // TODO 14)  memory leak search
-// DONE TODO 15) ADD SINGLETON tick counter
+// DONE 15) ADD SINGLETON tick counter
 // TODO 16) Create virtual dimension plugin
 // TODO 17) Create type Destroyable and check it function
